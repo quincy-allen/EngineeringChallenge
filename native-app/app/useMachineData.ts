0ebdb562f452c9227e82b1325c7dabb2e462a554
ignatius22 <allenquincy41@gmail.com>
2023-12-21 00:27:57 +0100

    Add redux for state management and persist data

--- native-app/app/useMachineData.ts
+++ native-app/app/useMachineData.ts
@@ -1,54 +1,18 @@
-import {useState, useEffect, useMemo, useCallback} from 'react';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useCallback } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { resetMachine, updateMachine } from "../redux/features/machineSlice";
 
 export const useMachineData = () => {
-  const [machineData, setMachineData] = useState(undefined);
-
-  useEffect(() => {
-    // Load machine data from local storage when the hook initializes
-    loadMachineData();
-  }, []);
-
-  const loadMachineData = useCallback(async () => {
-    try {
-      const storedMachineData = await AsyncStorage.getItem('machineData');
-
-      if (storedMachineData) {
-        // Parse stored machine data and set it in state
-        const parsedMachineData = JSON.parse(storedMachineData);
-        setMachineData(parsedMachineData);
-      } else {
-        setMachineData(undefined);
-      }
-    } catch (error) {
-      console.error(error);
-      // Handle storage loading error
-    }
-  }, []);
+  const dispatch = useDispatch();
+  const { machineData } = useSelector((state) => state.machines);
+  console.log(machineData, "machine data");
 
   const resetMachineData = useCallback(async () => {
-    try {
-      // Clear the machine data from local storage
-      await AsyncStorage.removeItem('machineData');
-      setMachineData(undefined);
-      // You can also clear other related data if needed
-    } catch (error) {
-      console.error(error);
-      // Handle storage clearing error
-    }
+    dispatch(resetMachine());
   }, []);
 
   const updateMachineData = useCallback(async (newMachineData) => {
-    try {
-      // Update the state with the new machine data
-      setMachineData(newMachineData);
-
-      // Persist the updated machine data to local storage
-      await AsyncStorage.setItem('machineData', JSON.stringify(newMachineData));
-    } catch (error) {
-      console.error(error);
-      // Handle storage saving error
-    }
+    dispatch(updateMachine(newMachineData));
   }, []);
 
   const setScores = useCallback(
@@ -63,26 +27,23 @@ export const useMachineData = () => {
         newMachineData.scores = newScores;
 
         // Update the state with the new machine data
-        setMachineData(newMachineData);
+        dispatch(updateMachine(newMachineData));
 
         // Persist the updated machine data to local storage
-        await AsyncStorage.setItem(
-          'machineData',
-          JSON.stringify(newMachineData),
-        );
+
+        JSON.stringify(newMachineData);
       } catch (error) {
         console.error(error);
         // Handle storage saving error
       }
     },
-    [machineData],
+    [machineData]
   );
 
   return {
     machineData,
     updateMachineData,
     resetMachineData,
-    loadMachineData,
     setScores,
   };
 };
